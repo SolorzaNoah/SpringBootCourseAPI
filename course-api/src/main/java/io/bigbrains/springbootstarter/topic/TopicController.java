d773ec91590d932f43e8cd46dd79641fc90a4b38
solorzanoah <sledgehacker@DESKTOP-S7H7PKI.localdomain>
2022-04-10 14:24:10 -0500

    Moved topics to @Service instead of a hard coded Array

--- course-api/src/main/java/io/bigbrains/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/bigbrains/springbootstarter/topic/TopicController.java
@@ -1,21 +1,20 @@
 package io.bigbrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired  //marks as something that needs dependency injection 
+	private TopicService topicService;		
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description"),
-				new Topic("javascript", "Javascript", "Javascript Description")
-				);
+		return topicService.getAllTopics();
 	}
 
 }
